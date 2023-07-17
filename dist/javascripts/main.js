cd8b89eb5351ecafa51a2fbd85075f298f0f940e
yugp0208 <yuu.fepnhdper2000@gmail.com>
2023-07-17 16:53:35 +0900

    追加のcommit

---
+++ dist/javascripts/main.js
@@ -0,0 +1,110 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/javascripts/main.js":
+/*!*********************************!*\
+  !*** ./src/javascripts/main.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _my_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./my.js */ "./src/javascripts/my.js");
+/* harmony import */ var _stylesheets_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../stylesheets/main.scss */ "./src/stylesheets/main.scss");
+
+
+console.log("webpack");
+(0,_my_js__WEBPACK_IMPORTED_MODULE_0__.default)();
+
+/***/ }),
+
+/***/ "./src/javascripts/my.js":
+/*!*******************************!*\
+  !*** ./src/javascripts/my.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function () {
+  console.log('this is modules');
+});
+
+/***/ }),
+
+/***/ "./src/stylesheets/main.scss":
+/*!***********************************!*\
+  !*** ./src/stylesheets/main.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/javascripts/main.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
 //webpackの設定ファイル
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //出力したCSSを別ファイルにするプラグイン
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //HTMLを出力するプラグイン
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //distフォルダに残ったゴミファイルを削除してくれる
 
 module.exports = {
-    entry : './src/index.js', //出力するファイル
+    mode: 'development', //本番公開用に、distにファイルを出力したい場合は、productionに設定する
+    devtool: 'source-map', //jsのデバッグ改善
+    entry : './src/javascripts/main.js', //出力するファイル
     output : {
         path : path.resolve(__dirname, './dist/'), //出力するディレクトリ //絶対パスを取得して指定
-        filename : 'main.js' //出力するファイルの名前を指定
+        filename : './javascripts/main.js', //出力するファイルの名前を指定
     },
     module : {
         rules:[
-            
             {
-                //.cssが見つかれば、useの中を適用するという指示
-                test: /\.css/,
+                //jsの為の記述
+                test: /\.js/,
+                exclude: '/node_modules/', //Babelでトランスパイルする必要のないフォルダの為、除外
+                use : [
+                    {
+                        loader:'babel-loader',
+                        options:{
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+            {
+                //scssとcssの為の記述
+                test: /\.(css|sass|scss)/,
                 use : [
                     //cssをバンドルする
                     {
@@ -22,16 +37,60 @@ module.exports = {
                     },
                     {
                         loader : 'css-loader',
+                        options : {
+                            sourceMap:false, //scssのデバック改善
+                        },
                     },
+                    {
+                        loader : 'sass-loader',
+                    }
                 ],
             },
-
+            {
+                //画像出力の為の記述
+                test:/\.(jpg|png|jpeg|webp)/,
+                type : 'asset/resource',
+                generator:{
+                    filename: 'images/[name][ext]'
+                },
+                use: [
+                    {
+                        loader : 'image-webpack-loader',
+                        options : {
+                            mozjpeg: {
+                                progressive : true,
+                                quality: 65
+                            },
+                            webp: {
+                                quality:75,
+                            }
+                        },
+                    }
+                ],
+            },
+            {
+                //pug・HTMLの為の記述
+                test: /\.html/,
+                use:[
+                    {
+                        loader:'html-loader',
+                    },
+                ]
+            }
         ],
     },
     plugins : [
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename : './stylesheets/main.css', //distフォルダの中身の命名を指定
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            filename: 'index.html',
         }),
+        new CleanWebpackPlugin(),
     ],
+    //npm-dev-serverの為の記述
+    devServer: {
+        static: path.resolve(__dirname, 'src'),
+    },
 }
